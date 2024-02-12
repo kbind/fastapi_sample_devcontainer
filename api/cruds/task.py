edf7996268d2ebd189aaa02e53f38cb1fe9c6ff5
kbind <kbind001@gmail.com>
2024-02-12 23:54:36 +0900

    DELETE

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -39,3 +39,7 @@ def update_task(db: Session, task_create: task_schema.TaskCreate, original: task
     db.commit()
     db.refresh(original)
     return original
+
+def delete_task(db: Session, original: task_model.Task) -> None:
+    db.delete(original)
+    db.commit()
